077d4a0862b6a2b38a06731f76bd9e5ed3290818
R-W03 <rwilliamson0356@floridapoly.edu>
2025-01-17 03:39:56 +0000

    Fixed version of 03

--- student_info.c/03_get_input/03_get_input.c
+++ student_info.c/03_get_input/03_get_input.c
@@ -2,22 +2,22 @@
 
 int main() {
 	int square_side = 1;
-	int square_perimeter = 0;
-	int square_area = 0;
+	int square_perimeter = 4;
+	int square_area = 1;
 	
     //Write the prompt
-    puts("Enter the side of a square")
+    puts("Enter the side of a square");
 
 
 	//Get the input
     // & - address operator
-	scanf("%d, &square_side");
+	scanf("%d", &square_side);
 
 	// Do the math
 	square_perimeter = square_side * 4;
 
 	//Create the output
-	printf("The perimeter of a square with side %d is %d\n", square_side, square_perimeter
+	printf("The perimeter of a square with side %d is %d\n", square_side, square_perimeter);
 
 	return 0;
 }
